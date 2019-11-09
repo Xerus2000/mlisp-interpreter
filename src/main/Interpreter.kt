9b9f0c9374dc31376099eb54ae2e6afa6480c671
Xerus <27jf@web.de>
2019-11-09 09:42:44 +0100

    feat: interpret recursively

--- src/main/Interpreter.kt
+++ src/main/Interpreter.kt
@@ -3,35 +3,34 @@ val availableFunctions = HashMap(defaultFunctions)
 fun interpret(code: String): Value {
     @Suppress("NAME_SHADOWING")
     val code = code.trim()
-    return if (code.startsWith('(')) {
-        if (code.last() != ')')
-            throw SyntaxError(code, "Missing closing parenthesis!")
-        val expression = code.substring(1, code.lastIndex)
-        if (code[1].isDigit() || code[1] == '-')
-            interpretList(expression)
-        else
-            interpretFunction(expression)
-    } else {
-        parseLiteral(code)
-    }
+    return interpretRecursively(code).value
 }
 
-fun interpretList(expression: String): ListValue {
-    return interpretRecursively(expression).value as ListValue
-}
+fun isLiteral(first: Char) = first.isDigit() || first == '-'
 
 fun parseLiteral(expression: String): IntValue =
     IntValue(expression.toIntOrNull() ?: throw SyntaxError(expression, "Expression is not an Integer!"))
 
 data class InterpretResult(val value: Value, val remainder: String)
-fun interpretRecursively(expression: String): InterpretResult {
+
+fun interpretRecursively(code: String): InterpretResult {
     val values = arrayListOf<Value>()
-    var remainder = expression
+    var remainder = code
     while (remainder.isNotEmpty()) {
         if (remainder[0] == '(') {
-            val result = interpretRecursively(remainder.substring(1))
-            remainder = result.remainder
-            values.add(result.value)
+            val expression = remainder.substring(1)
+            if (isLiteral(expression[0]) || expression[0] == '(') {
+                val result = interpretRecursively(expression)
+                remainder = result.remainder
+                values.add(result.value)
+            } else {
+                val split = expression.split(' ', limit = 2)
+                val function = availableFunctions[split[0]] ?: throw FunctionNotDefinedError(split[0])
+                val result = interpretRecursively(split[1])
+                remainder = result.remainder
+                val arguments = result.value.asList().toTypedArray()
+                values.add(function.invoke(arguments))
+            }
         } else {
             val split = remainder.split(' ', limit = 2)
             val currentValue = split[0]
@@ -44,14 +43,5 @@ fun interpretRecursively(expression: String): InterpretResult {
             }
         }
     }
-    return InterpretResult(ListValue(values), remainder)
+    return InterpretResult(if(code[0] == '(') values[0] else ListValue(values), remainder)
 }
-
-fun interpretFunction(code: String): Value {
-    val split = code.split(' ', limit = 2)
-    val function = availableFunctions[split[0]]
-    val arguments = interpretList(split[1]).value.toTypedArray()
-    return function?.invoke(arguments)
-        ?: throw FunctionNotDefinedError(split[0])
-}
-
--- src/test/InterpreterTest.kt
+++ src/test/InterpreterTest.kt
@@ -3,9 +3,6 @@ import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
 
 class InterpreterTest : StringSpec({
-    "Int with Padding" {
-        IntValue(3) shouldBe interpret(" 3 ")
-    }
     "List" {
         val list = listOf(-1, 2, 3)
         val listLiteral = interpret("(-1 2 3)")
@@ -29,9 +26,14 @@ class InterpreterTest : StringSpec({
     "Method extension" {
         ListValue(1, 4, 6, 5, 19) shouldBe interpret("(append (1 4 6) 5 19)")
     }
+    "Multi-Dimensional Lists" {
+        ListValue(ListValue(1)) shouldBe interpret("((1))")
+        ListValue(ListValue(1, 5), ListValue(2, 3)) shouldBe interpret("((1 5) (2 3))")
+    }
     "Catches Syntax errors" {
         shouldThrow<SyntaxError> { interpret("= 1 2") }
         shouldThrow<SyntaxError> { interpret("(= 1 2") }
         shouldThrow<InvalidArgumentNumberError> { interpret("(= 1 2 3)") }
+        shouldThrow<InvalidArgumentNumberError> { interpret("first (1 2) 3") }
     }
 })
